2e75bd7db6e3b22343a565d8ec5bb2b57318b834
Maya Shavin <maya@cloudinary.com>
2021-01-27 10:19:12 +0200

    feat: add CRUD methods to server api

--- src/runtime/server.ts
+++ src/runtime/server.ts
@@ -1,9 +1,94 @@
-import type { Asset, CloudConfig } from '@cld-apis/types'
+import type { Asset, CloudConfig, ResourceType } from '@cld-apis/types'
 import { toSnakeCase } from '@cld-apis/utils'
+import { DeliveryType } from 'cloudinary'
 import CloudinaryApi from './api'
 
 export type CldError = {
   msg: string
+}
+
+export type Options = {
+  resourceType?: ResourceType;
+  type?: DeliveryType;
+  invalidate?: boolean;
+}
+
+export type SingleDeleteOptions = Options
+
+export type RenameOptions = Options & {
+  toType: DeliveryType;
+  overwrite: boolean;
+}
+
+export type AccessMode = 'public' | 'authenticated';
+
+export type AccessControlType = 'token' | 'anonymous'
+
+export type AccessControl = {
+  type: AccessControlType;
+  start?: string;
+  end?: string;
+}
+
+export type ResponsiveBreakpointOption = {
+  createDerived: boolean;
+  format?: string;
+  transformation?: string;
+  maxWidth?: number;
+  minWidth?: number;
+  bytesStep?: number;
+  maxImages?: number
+}
+
+export type ResourceDataOptions = {
+  tags?: string[]
+  context?: string[]
+  colors?: boolean
+  faces?: boolean;
+  qualityAnalysis?: boolean;
+  accessibilityAnalysis?: boolean;
+  cinemagraphAnalysis?: boolean;
+  imageMetadata?: boolean;
+  phash?: boolean
+  responsiveBreakpoints?: ResponsiveBreakpointOption[]
+  autoTagging?: number;
+  categorization?: string;
+  detection?: string;
+  ocr?: string;
+  exif?: boolean;
+}
+
+export type ManupulationOptions = {
+  eager?: string[];
+  eageAsync?: boolean;
+  eagerNotificationUrl?: string
+  transformation?: string
+  format?: string;
+  customCoordinates?: string[]
+  faceCoordinates?: string[]
+  backgroundRemoval?: 'cloudinary_ai' | 'pixelz'
+  rawConvert?: 'apose' | 'google_speech' | 'extract_text'
+}
+
+export type UploadOptions = Options & ManupulationOptions & ResourceDataOptions & {
+  publicId?: string;
+  folder?: string;
+  useFilename?: boolean;
+  uniqueFilename?: boolean;
+  accessControl?: AccessControl;
+  accessMode?: AccessMode;
+  discardOriginalFilename?: boolean;
+  overwrite?: boolean;
+  allowedFormats?: string[];
+  async?: boolean;
+  backup?: boolean;
+  eval?: string;
+  headers?: string;
+  invalidate?: boolean;
+  moderation?: string;
+  notificationUrl?: string;
+  proxy?: string;
+  returnDeleteToken?: boolean;
 }
 
 export class ServerApi extends CloudinaryApi {
@@ -21,7 +106,7 @@ export class ServerApi extends CloudinaryApi {
    * @param {Function} callback - callback handler (optional)
    * @returns {Promise<Asset | CldError>}
    */
-  upload (file: string, options: Object = {}, callback?: Function):Promise<Asset | CldError> {
+  upload (file: string, options: UploadOptions = {}, callback?: Function):Promise<Asset | CldError> {
     const $options = toSnakeCase(options)
 
     const cldServer = require('cloudinary').v2
@@ -39,7 +124,17 @@ export class ServerApi extends CloudinaryApi {
    * @param {{ type: {DeliveryType} }} options - options to apply on the asset : https://cloudinary.com/documentation/image_upload_api_reference#optional_parameters-6
    * @returns {Promise<Asset | CldError | null>}
    */
-  async explicit (publicId: string, options:Object = {}):Promise<Asset | CldError> {
+  explicit (publicId: string, options:Object = {}):Promise<Asset | CldError> {
+    return this.resource(publicId, options)
+  }
+
+  /**
+   *
+   * @param {String} publicId - the public id of the target asset
+   * @param {{ type: {DeliveryType} }} options - options to apply on the asset : https://cloudinary.com/documentation/image_upload_api_reference#optional_parameters-6
+   * @returns {Promise<Asset | CldError | null>}
+   */
+  async resource (publicId: string, options:Object = {}):Promise<Asset | CldError> {
     const $options = toSnakeCase(options)
 
     const cldServer = require('cloudinary').v2
@@ -49,6 +144,34 @@ export class ServerApi extends CloudinaryApi {
     const uploader = cldServer.uploader
 
     const asset:Promise<Asset | CldError> = await uploader.explicit(publicId, $options)
+
+    return asset
+  }
+
+  async delete (publicId: string, options: SingleDeleteOptions):Promise<{ result: string } | CldError> {
+    const $options = toSnakeCase(options)
+
+    const cldServer = require('cloudinary').v2
+
+    cldServer.config(toSnakeCase(this.configurations))
+
+    const uploader = cldServer.uploader
+
+    const result:Promise<{ result: string } | CldError> = await uploader.destroy(publicId, $options)
+
+    return result
+  }
+
+  async rename (currPublicId: string, newPublicId, options: RenameOptions):Promise<Asset | CldError> {
+    const $options = toSnakeCase(options)
+
+    const cldServer = require('cloudinary').v2
+
+    cldServer.config(toSnakeCase(this.configurations))
+
+    const uploader = cldServer.uploader
+
+    const asset:Promise<Asset | CldError> = await uploader.rename(currPublicId, newPublicId, $options)
 
     return asset
   }
