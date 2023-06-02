ac84ab48945a3d27cf3e2e27ad79279b235f8f98
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-06-02 13:37:05 +0200

    bump: 2.0.0-rc.3

--- src/runtime/components/CldImage.vue
+++ src/runtime/components/CldImage.vue
@@ -1,6 +1,40 @@
 <script setup lang="ts">
-import type { AssetOptions } from '@cloudinary-util/url-loader'
+// This is working in dev playground but does not work in the published package
+// Come back to this after https://github.com/nuxt/nuxt/issues/20936 is fixed
+// import type { AssetOptions } from '@cloudinary-util/url-loader'
 import { useCldImageUrl } from '../composables/useCldImageUrl';
+
+interface AssetOptionsResize {
+    crop?: string;
+    width?: number | string;
+}
+
+interface AssetOptions {
+    assetType?: string;
+    crop?: string;
+    deliveryType?: string;
+    effects?: Array<any>;
+    flags?: Array<string> | object;
+    format?: string;
+    gravity?: string;
+    height?: string | number;
+    overlays?: Array<any>;
+    quality?: number | string;
+    rawTransformations?: string[];
+    removeBackground?: boolean;
+    sanitize?: boolean;
+    resize?: AssetOptionsResize;
+    seoSuffix?: string;
+    src: string;
+    text?: any;
+    transformations?: Array<string>;
+    underlay?: string;
+    underlays?: Array<any>;
+    version?: number | string;
+    width?: string | number;
+    widthResize?: string | number;
+    zoom?: string;
+}
 
 interface CldImageProps {
   loading?: "eager" | "lazy";
