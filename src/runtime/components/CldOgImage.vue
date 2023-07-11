7b732efe68634ea9523e5ed434a36a3a0eea8dfd
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-07-11 10:20:27 +0200

    fix: types * 2.1.5

--- src/runtime/components/CldOgImage.vue
+++ src/runtime/components/CldOgImage.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { CldImageProps } from "./CldImage.vue";
 import { useCldImageUrl } from "../composables/useCldImageUrl";
 import { computed } from 'vue'
 import { useRouter } from "#imports";
@@ -10,6 +9,64 @@ const OG_IMAGE_WIDTH_RESIZE = 1200;
 const OG_IMAGE_HEIGHT = 1254;
 
 const { currentRoute } = useRouter()
+
+// Same story as with CldImage.vue component. Cannot use imported types for props
+// Come back to this after https://github.com/nuxt/nuxt/issues/20936 is fixed
+interface AssetOptionsResize {
+  crop?: string;
+  width?: number | string;
+}
+
+interface AssetOptions {
+  assetType?: string;
+  crop?: string;
+  deliveryType?: string;
+  effects?: Array<any>;
+  flags?: Array<string> | object;
+  format?: string;
+  gravity?: string;
+  height?: string | number;
+  overlays?: Array<any>;
+  quality?: number | string;
+  rawTransformations?: string[];
+  removeBackground?: boolean;
+  sanitize?: boolean;
+  resize?: AssetOptionsResize;
+  seoSuffix?: string;
+  src: string;
+  text?: any;
+  transformations?: Array<string>;
+  underlay?: string;
+  underlays?: Array<any>;
+  version?: number | string;
+  width?: string | number;
+  widthResize?: string | number;
+  zoom?: string;
+}
+
+interface ImageOptionsZoomPan {
+  loop: string | boolean;
+  options: string;
+}
+interface ImageOptions extends AssetOptions {
+  zoompan?: string | boolean | ImageOptionsZoomPan;
+}
+
+export interface CldImageProps extends ImageOptions {
+  loading?: "eager" | "lazy";
+  fetchPriority?: "high" | "low" | "auto";
+  // Adding below as required props to promote good patterns in developing images
+  alt: string;
+  width: string | number;
+  height: string | number;
+  // Cloudinary URL Loader props
+  // Cannot use `ConfigOptions` due to the same issue as mentioned at the top
+  config?: any;
+  // Unpic props
+  layout?: "constrained" | "fullWidth" | "fixed";
+  priority?: boolean;
+  background?: "auto" | string;
+}
 
 export type CldOgImageProps = CldImageProps & {
   excludeTags?: Array<string>;
