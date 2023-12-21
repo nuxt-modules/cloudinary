0258d70a0f8a63fcdc5439ecf843de3eb9ccdf1e
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-12-21 14:52:02 +0100

    docs: update documentation for CldImage

---
+++ docs/components/content/ImageWithRecolor.vue
@@ -0,0 +1,10 @@
+<template>
+  <CldImage
+    src="images/sneakers"
+    width="900"
+    height="900"
+    alt="Sample Product"
+    crop="fill"
+    :recolor="['shoelaces', 'purple']"
+  />
+</template>
