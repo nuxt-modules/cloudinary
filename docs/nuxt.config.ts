c7365cf76cb47bc789cabbeaed627ab866c13b29
Baroshem <jakub.andrzejewski.dev@gmail.com>
2024-01-23 14:02:51 +0100

    refactor: docs

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,17 +1,46 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus',
-  modules: ['@nuxtjs/cloudinary'],
+  extends: '@nuxt/ui-pro',
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxtjs/cloudinary',
+  ],
+
+  colorMode: {
+    preference: 'dark',
+  },
+
   cloudinary: {
     cloudName: 'nuxt-cloudinary'
   },
 
-  postcss: {
-    plugins: {
-      autoprefixer: {},
-     cssnano:
-       process.env.NODE_ENV === 'production'
-         ? { preset: ['default', { discardComments: { removeAll: true } }] }
-         : false, // disable cssnano when not in production
+  ui: {
+    icons: ['heroicons', 'simple-icons', 'ph'],
+  },
+
+  nitro: {
+    prerender: {
+      routes: ['/api/search.json'],
     },
- }
+  },
+
+  hooks: {
+    // Related to https://github.com/nuxt/nuxt/pull/22558
+    // Adding all global components to the main entry
+    // To avoid lagging during page navigation on client-side
+    'components:extend': function (components) {
+      for (const comp of components) {
+        if (comp.global)
+          comp.global = 'sync'
+      }
+    },
+  },
+
+  // devtools: {
+  //   enabled: true,
+  //   componentInspector: {
+  //     cleanHtml: false,
+  //   },
+  // },
 })
