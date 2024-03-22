5a21810da7d835b33b25382ee6171add61d03cee
Colby Fayock <fayock@gmail.com>
2024-03-22 14:18:24 -0300

    adding ability to pass in global module configurations

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   cloudinary: {
-    cloudName: 'nuxt-cloudinary'
+    cloudName: 'nuxt-cloudinary',
+    url: {
+      cname: 'spacejelly.dev',
+      secureDistribution: 'spacejelly.dev',
+      secure: true,
+      privateCdn: true
+    }
   }
 })
