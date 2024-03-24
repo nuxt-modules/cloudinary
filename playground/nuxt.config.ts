8ad2df694ec8e3b5955663c51a889bd452ba9c68
Baroshem <jakub.andrzejewski.dev@gmail.com>
2024-03-24 19:24:47 +0100

    feat-#197: useCldVideoUrl composable

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,11 +2,7 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   cloudinary: {
     cloudName: 'nuxt-cloudinary',
-    url: {
-      cname: 'spacejelly.dev',
-      secureDistribution: 'spacejelly.dev',
-      secure: true,
-      privateCdn: true
-    }
+    url: {},
+    cloud: {}
   }
 })
---
+++ src/runtime/composables/useCldVideoUrl.ts
@@ -0,0 +1,47 @@
+import { useRuntimeConfig } from '#imports'
+import { constructCloudinaryUrl, ConstructUrlProps, ConfigOptions, AnalyticsOptions, VideoOptions } from '@cloudinary-util/url-loader'
+import nuxtPkg from 'nuxt/package.json';
+import pkg from '../../../package.json'
+
+export const useCldVideoUrl = (props: { options: VideoOptions, config?: ConfigOptions, analytics?: AnalyticsOptions }) => {
+  if (!props.options.src) console.error("`[@nuxtjs/cloudinary]`: Property `src` is missing")
+
+  const moduleConfig = useRuntimeConfig().public.cloudinary;
+
+  const cldCloudName = props.config?.cloud?.cloudName || moduleConfig.cloud?.cloudName || moduleConfig?.cloudName;
+
+  if (!cldCloudName) console.warn('`[@nuxtjs/cloudinary]` Environment variable `CLOUDINARY_CLOUD_NAME` or property `cloudinary.cloudName` missing')
+
+  let cldOptions: ConstructUrlProps = {
+    options: {
+      assetType: 'video',
+      // Have to spread the options because otherwise getting the error about props being updated while they are readonly.
+      ...props.options
+    },
+    config: {
+      url: moduleConfig.url,
+      cloud: {
+        cloudName: cldCloudName,
+        ...moduleConfig.cloud
+      },
+      ...props.config,
+    },
+    analytics: false
+  }
+
+  if (moduleConfig.analytics) {
+    cldOptions = {
+      ...cldOptions,
+      analytics: Object.assign({
+        sdkCode: 'D',
+        sdkSemver: `${pkg.version.split('.')[0]}.0.0`,
+        techVersion: `${nuxtPkg.version.split('.')[0]}.0.0`,
+        product: 'B'
+      }, props.analytics)
+    }
+  }
+
+  return {
+    url: constructCloudinaryUrl(cldOptions)
+  }
+}
