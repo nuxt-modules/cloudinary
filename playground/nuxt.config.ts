81c6690e8936fdd37a39cd8dc66deb897c8b8af4
Jakub Andrzejewski <jakub.andrzejewski.dev@gmail.com>
2024-03-24 10:44:49 -0700

    Update nuxt.config.ts

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,11 +2,5 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   cloudinary: {
     cloudName: 'nuxt-cloudinary',
-    url: {
-      cname: 'spacejelly.dev',
-      secureDistribution: 'spacejelly.dev',
-      secure: true,
-      privateCdn: true
-    }
   }
 })
